d42e1c44c2d66f370123810f4f83bf4e6b8760fd
FoxWeb <dneprfox@gmail.com>
2020-02-09 17:55:01 +0200

    index.html ready

---
+++ app/js/main.js
@@ -0,0 +1,72 @@
+$(function(){
+
+  $('.menu__btn').on("click", function(){
+    $(this).toggleClass('open');
+    $('.menu__list').slideToggle();
+  });
+ 
+
+  ////header__slider////////
+  $('.header__slider-inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true   
+  });
+  ////worked-slider////////
+  $('.worked__slider-items').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<div class="prev"><i class="fas fa-chevron-left"></i></div>',
+    nextArrow: '<div class="next"><i class="fas fa-chevron-right"></i></div>', 
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,         
+        }
+      },  
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,          
+        }
+      },         
+      
+    ]
+  });
+  
+  // product__slider
+  $('.product__slider-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,    
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    prevArrow: '<div class="prev"><i class="fas fa-chevron-left"></i></div>',
+    nextArrow: '<div class="next"><i class="fas fa-chevron-right"></i></div>',
+    fade: true,    
+    dots: true   
+  });
+   ////clients__slider////////
+   $('.clients__slider-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,  
+    arrows: false,       
+    dots: false
+  });
+  
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
+ 
+
+  
+});
