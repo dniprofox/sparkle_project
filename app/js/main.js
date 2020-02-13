ad1104e7dda9d8a85afd977f844f7bc203090513
FoxWeb <dneprfox@gmail.com>
2020-02-13 18:02:15 +0200

    ready about.html

--- app/js/main.js
+++ app/js/main.js
@@ -65,8 +65,44 @@ $(function(){
   
   $('.header__btn-menu').on('click', function () {
     $('.header__box').toggleClass('active');
-  });
+  }); 
  
+    $(".num").counterUp({
+    delay:10,
+    time:2000
+    });
 
+  
+ 
+  $(function() {
+		var Accordion = function(el, multiple) {
+				this.el = el || {};
+				this.multiple = multiple || false;
+
+				var links = this.el.find('.article-title');
+				links.on('click', {
+						el: this.el,
+						multiple: this.multiple
+				}, this.dropdown)
+		}
+
+		Accordion.prototype.dropdown = function(e) {
+				var $el = e.data.el;
+				$this = $(this),
+						$next = $this.next();
+
+				$next.slideToggle();
+				$this.parent().toggleClass('open');
+
+				if (!e.data.multiple) {
+						$el.find('.accordion-content').not($next).slideUp().parent().removeClass('open');
+				};
+		}
+		var accordion = new Accordion($('.accordion-container'), false);
+});
+ 
+  
+  /////////////////////////////////
+  
   
 });
